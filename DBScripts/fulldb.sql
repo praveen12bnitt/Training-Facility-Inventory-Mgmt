c270b883d23964e49b0f6ea1639df38aba693d55
Palanivelrajan Balasubramanian <praveen12bnitt@gmail.com>
2011-11-01 19:35:57 -0400

    added transaction item mapping table, which was missed in the previous commit

--- DBScripts/fulldb.sql
+++ DBScripts/fulldb.sql
@@ -116,6 +116,24 @@ INSERT INTO `transaction_type` (`transaction_type`,`location_id`,`transaction_ty
 INSERT INTO `transaction_type` (`transaction_type`,`location_id`,`transaction_type_desc`) VALUES ('ISSUE_UNIFORM_STUDENT',2,'Issue Uniform to Students');
 INSERT INTO `transaction_type` (`transaction_type`,`location_id`,`transaction_type_desc`) VALUES ('RETURN_UNIFORM_STUDENT',2,'Uniform returns by Students');
 
+-- Transaction item mapping
+
+CREATE TABLE `transaction_item_mapping` (
+  `transaction_type` varchar(45) NOT NULL,
+  `ITEM_ID` int(11) NOT NULL,
+  `UI_WEIGHT` int(11) NOT NULL,
+  PRIMARY KEY (`transaction_type`,`ITEM_ID`),
+  KEY `item_fk` (`ITEM_ID`),
+  KEY `trans_type_fk` (`transaction_type`),
+  KEY `transFk` (`transaction_type`),
+  CONSTRAINT `transFk` FOREIGN KEY (`transaction_type`) REFERENCES `transaction_type` (`transaction_type`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `item_fk` FOREIGN KEY (`ITEM_ID`) REFERENCES `item` (`ITEM_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+INSERT INTO `transaction_item_mapping` (`transaction_type`,`ITEM_ID`,`UI_WEIGHT`) VALUES ('ISSUE_UNIFORM_STUDENT',1,1);
+INSERT INTO `transaction_item_mapping` (`transaction_type`,`ITEM_ID`,`UI_WEIGHT`) VALUES ('ISSUE_UNIFORM_STUDENT',2,1);
+
 -- Inventory table--Transaction table...so no data to fill
 
 CREATE TABLE `inventory` (
